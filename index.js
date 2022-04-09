4d247bfe66babddc034c6526e76bb6473a2dce48
tallesChristian <72113346+tallesChristian@users.noreply.github.com>
2022-04-09 08:51:45 -0300

    root

--- index.js
+++ index.js
@@ -1,68 +1,50 @@
-let botao = document.querySelectorAll(".botao");
-let display = document.getElementById("resultado");
-let valorDisplay = display.textContent;
+const display = document.getElementById("resultado");
+const numeros = document.querySelectorAll("[id*= botao]");
+const operadores = document.querySelectorAll("[id*= tecla]");
+const limpar = document.querySelectorAll("#limpar");
 
+let novoNumero = true;
+let operador;
+let numeroAnterior;
 
+const operacaoPnedente = () => operador !== undefined;
 
-for (let i = 0; i < botao.length; i++) {
-    botao[i].addEventListener('click', (event) => {
+const calcular = () => {
+    if (operacaoPnedente()) {
+        novoNumero = true;
+        const numeroAtual = parseFloat(display.textContent);
+        const resultado = eval(`${numeroAnterior} ${operador} ${numeroAtual}`);
+        atualizarDisplay(resultado);
 
-        var valor = botao[i];
-        display.textContent += valor.textContent;
-    })
+    }
 }
 
-let botaoC = document.querySelectorAll("#botao__c");
-for (let i = 0; i < botaoC.length; i++) {
-    botaoC[i].addEventListener('click', (event) => {
+const atualizarDisplay = (texto) => {
+    if (novoNumero) {
+        display.textContent = texto;
+        novoNumero = false
+    } else { display.textContent += texto; }
 
-        display.textContent = "";
-
-    })
 }
+const inserirNumero = (evento) => atualizarDisplay(evento.target.textContent);
+numeros.forEach(numero => numero.addEventListener("click", inserirNumero));
 
-let botaoDiv = document.querySelectorAll("#botao__divisao");
-for (let i = 0; i < botaoC.length; i++) {
-    botaoDiv[i].addEventListener('click', () => {
-        display.textContent += "/";
-
-    })
+const limpaDisplay = () => {
+    novoNumero = true;
+    atualizarDisplay("");
 }
-let botaoIgual = document.querySelectorAll("#botao__igual");
-for (let i = 0; i < botaoIgual.length; i++) {
-    botaoIgual[i].addEventListener('click', () => {
-        function Soma(textContent) {
-            var DECIMAL = 10;
-            return parseInt(valorDisplay);
-        }
-    })
+limpar.forEach(limpa => limpa.addEventListener("click", limpaDisplay));
+
+
+
+function selecionarOperador(evento) {
+    if (!novoNumero) {
+        calcular();
+        novoNumero = true;
+        operador = evento.target.textContent;
+        numeroAnterior = parseFloat(display.textContent);
+    }
+
+
 }
-
-let botaoMais = document.querySelectorAll("#botao__mais");
-for (let i = 0; i < botaoIgual.length; i++) {
-    botaoMais[i].addEventListener('click', () => {
-        valorDisplay += valorDisplay + "+";
-        console.log(valorDisplay)
-    })
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-////for (i = 0; i < botao.length; i++) {
-//    botao.addEventListener("click", function() {
-//        console.log("clickei");
-//    })
-//}
+operadores.forEach(operador => operador.addEventListener("click", selecionarOperador));
